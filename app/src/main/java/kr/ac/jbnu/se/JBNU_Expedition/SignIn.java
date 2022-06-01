44661f543e9c2c0c76d2f36b69dbb4962da61c61
HyeSunJeong <zaqxswcde88@naver.com>
2022-06-01 22:32:17 +0900

    sign-up 화면 구현 및 firebase 연결

--- app/src/main/java/kr/ac/jbnu/se/JBNU_Expedition/SignIn.java
+++ app/src/main/java/kr/ac/jbnu/se/JBNU_Expedition/SignIn.java
@@ -3,39 +3,62 @@ package kr.ac.jbnu.se.JBNU_Expedition;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignIn extends AppCompatActivity {
 
     // 변수 선언
-    ImageButton sign_in_btn;
-    TextView sign_up_btn;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+    private EditText signInEmail;
+    private EditText signInPassword;
+    private ImageButton signInBtn;
+    private TextView signUpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in);
+        FirebaseApp.initializeApp(this);
 
         // 변수와 xml 파일의 id 연결
-        sign_in_btn = findViewById(R.id.sign_in_btn);
-        sign_up_btn = findViewById(R.id.sign_up_btn);
+        firebaseAuth = FirebaseAuth.getInstance();
 
-        // 로그인 버튼
-        sign_in_btn.setOnClickListener(new View.OnClickListener() {
+        signInEmail = (EditText) findViewById(R.id.sign_in_email);
+        signInPassword = (EditText) findViewById(R.id.sign_in_password);
+
+        signInBtn = (ImageButton) findViewById(R.id.sign_in_btn);
+        signUpBtn = (TextView) findViewById(R.id.sign_up_btn);
+
+
+
+        // 로그인 버튼: 공백 구별
+        signInBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Map.class);
-                startActivity(intent);
+                if (!signInEmail.getText().toString().equals("") && !signInPassword.getText().toString().equals("")) {
+                    loginUser(signInEmail.getText().toString(), signInPassword.getText().toString());
+                } else {
+                    Toast.makeText(SignIn.this, "이메일과 비밀번호를 입력하세요.", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
         // 회원가입 버튼
-        sign_up_btn.setOnClickListener(new View.OnClickListener() {
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), SignUp.class);
@@ -43,4 +66,40 @@ public class SignIn extends AppCompatActivity {
             }
         });
     }
+
+    // 로그인 버튼: 성공 및 실패
+    public void loginUser(String email, String password) {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // 로그인 성공
+                            Toast.makeText(SignIn.this, "로그인 성공", Toast.LENGTH_SHORT).show();
+                            firebaseAuth.addAuthStateListener(firebaseAuthListener);
+                            Intent intent = new Intent(getApplicationContext(), Map.class);
+                            startActivity(intent);
+
+                        } else {
+                            // 로그인 실패
+                            Toast.makeText(SignIn.this, "아이디 또는 비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    // 파이어베이스 실행, 중지
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (firebaseAuthListener != null) {
+            firebaseAuth.removeAuthStateListener(firebaseAuthListener);
+        }
+    }
+
 }
--- app/src/main/java/kr/ac/jbnu/se/JBNU_Expedition/SignUp.java
+++ app/src/main/java/kr/ac/jbnu/se/JBNU_Expedition/SignUp.java
@@ -1,14 +1,91 @@
 package kr.ac.jbnu.se.JBNU_Expedition;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import androidx.annotation.Nullable;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import androidx.annotation.Nullable;
+
 public class SignUp extends AppCompatActivity {
+
+    private ImageButton joinBtn;
+    private ImageView signUpBack;
+
+    private EditText signUpId;
+    private EditText signUpEmail;
+    private EditText signUpPassword;
+
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
+
+        // 변수와 xml 파일의 id 연결
+        joinBtn = (ImageButton) findViewById(R.id.join_btn);
+        signUpBack = (ImageView) findViewById(R.id.sign_up_back);
+
+        signUpId = (EditText) findViewById(R.id.sign_up_id);
+        signUpEmail = (EditText) findViewById(R.id.sign_up_email);
+        signUpPassword = (EditText) findViewById(R.id.sign_up_password);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        // 회원가입: 공백 구별
+        joinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!signUpEmail.getText().toString().equals("") && !signUpPassword.getText().toString().equals("")) {
+                    // 이메일과 비밀번호가 공백이 아닌 경우
+                    createUser(signUpEmail.getText().toString(), signUpEmail.getText().toString(), signUpId.getText().toString());
+                } else {
+                    // 이메일과 비밀번호가 공백인 경우
+                    Toast.makeText(SignUp.this, "이메일과 비밀번호를 입력하세요.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        // 회원 가입 창에서 뒤로가기 버튼 누르면, 로그인 화면으로 이동
+        signUpBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignIn.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    // 회원가입: 성공 및 실패
+    private void createUser(String email, String password, String name) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // 회원가입 성공시
+                            Toast.makeText(SignUp.this, name + "님, 환영합니다!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), SignIn.class);
+                            startActivity(intent);
+                        } else {
+                            // 계정이 중복된 경우
+                            Toast.makeText(SignUp.this, "이미 등록된 계정입니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
